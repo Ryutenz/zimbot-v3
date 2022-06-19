f936ad8223282975a99e5059b5f451e797c8c4dc
Ryutenz <98021671+Ryutenz@users.noreply.github.com>
2022-06-19 08:34:09 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','447441437150']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['6283819082780','14053494665']
+global.premium = ['6283819082780','14053494665']
+global.botname = 'Ryu Bot'
+global.ownername = 'Lucyfxr'
+global.group1 = 'https://chat.whatsapp.com/GsuP6Mw4jjs7qm4XtXkwAd'
+global.group2 = 'https://chat.whatsapp.com/IcbwpDNV2aNJqLMfh3pfFz'
+global.packname = 'Lucyfxr'
+global.ytchannel = 'http://botwa.rf.gd/'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
